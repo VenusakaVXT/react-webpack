c71840377c6b0af79b8d9ee6d4529de7056718fa
VenusakaVXT <voxuantuan33@gmail.com>
2023-09-05 21:41:08 +0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        // The directory containing the file to be built
+        path: path.join(__dirname, "/build"),
+        // The name of the file to be built
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Will use babel-loader for .js files
+                exclude: /node_modules/, // Exclude the node_modules folder
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/, // Use style-loader, css-loader for .css files
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    // Contains plugins to install in the future
+    plugins: [
+    ]
+}
